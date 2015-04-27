2f1ecbab44b740a6e40a5e87fd7d81347aa402b7
grayworm <fullyou-210@163.com>
2015-04-27 21:38:57 +0800

    update 注释

--- event/mc-signal/src/main/scala/com/asiainfo/ocdc/streaming/CityRule.scala
+++ event/mc-signal/src/main/scala/com/asiainfo/ocdc/streaming/CityRule.scala
@@ -10,12 +10,25 @@ class CityRule extends MCLabelRule{
     // 根据imsi 解析出用户开户地市
     val cityid = cityAnalysis(imsi)
     val propMap = scala.collection.mutable.Map[String, String]()
-    mcSourceObj.setLabel(Constant.lABEL_CITY, scala.collection.mutable.Map(cityid -> "true"))
+    mcSourceObj.setLabel(Constant.lABEL_CITY, scala.collection.mutable.Map("code" -> cityid))
     println(" set cityid label !!! ")
     propMap.iterator.foreach(x => {
       println(" cityid : " + x._1 + " flag : " + x._2)
     })
+
+
+
+    // 根据imsi 解析出用户开户区县
+    val countyid = countyAnalysis(imsi)
+    val propMap2 = scala.collection.mutable.Map[String, String]()
+    mcSourceObj.setLabel(Constant.lABEL_COUNTY, scala.collection.mutable.Map("code" -> countyid))
+    println(" set coutyid label !!! ")
+    propMap2.iterator.foreach(x => {
+      println(" countyid : " + x._1 + " flag : " + x._2)
+    })
+
     cache
+
   }
 
   /**
@@ -25,6 +38,12 @@ class CityRule extends MCLabelRule{
    */
   def cityAnalysis(imsi :String): String = {
     val cityid = CacheFactory.getManager.getCommonCacheValue("userinfo", imsi)
-    if(cityid == null || cityid.isEmpty)  null else cityid
+    if(cityid == null || cityid.isEmpty)  null else cityid.split(",")(0)
   }
+
+  def countyAnalysis(imsi :String): String = {
+    val countyid = CacheFactory.getManager.getCommonCacheValue("userinfo", imsi)
+    if(countyid == null || countyid.isEmpty)  null else countyid.split(",")(1)
+  }
+
 }
