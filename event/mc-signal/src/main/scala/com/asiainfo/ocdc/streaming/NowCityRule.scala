12946871a6744417ecf93470f036ffa9634fa07d
grayworm <fullyou-210@163.com>
2015-04-27 19:55:31 +0800

    Constant 增加 开户地市和区县和当前所在地市 常量

---
+++ event/mc-signal/src/main/scala/com/asiainfo/ocdc/streaming/NowCityRule.scala
@@ -0,0 +1,34 @@
+package com.asiainfo.ocdc.streaming
+
+/**
+ * Created by liugang on 15-4-27.
+ *
+ */
+class NowCityRule extends MCLabelRule{
+  def attachMCLabel(mcSourceObj: MCSourceObject, cache: StreamingCache): StreamingCache = {
+    val lac = mcSourceObj.lac
+    val ci = mcSourceObj.ci
+
+    // 根据largeCell解析出当前所在地市
+    val nowcityid = largeCellAnalysis(lac, ci)
+    val propMap = scala.collection.mutable.Map[String, String]()
+    //onsiteList.foreach(location => propMap += (location -> "true"))
+    mcSourceObj.setLabel(Constant.lABEL_NOW_CITY, scala.collection.mutable.Map(nowcityid -> "true"))
+    println(" set site label !!! ")
+    propMap.iterator.foreach(x => {
+      println(" area : " + x._1 + " flag : " + x._2)
+    })
+    cache
+  }
+
+  /**
+   * 根据largeCell解析出当前所在地市
+   * @param lac:MC信令代码
+   * @param ci:MC信令代码
+   * @return 当前所在地市
+   */
+  def largeCellAnalysis(lac: String, ci: String): String = {
+    val nowcityid = CacheFactory.getManager.getCommonCacheValue("btsinfo", lac+":"+ci)
+    if(nowcityid == null || nowcityid.isEmpty)  null else nowcityid
+  }
+}
